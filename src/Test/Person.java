c3cd19bdee68f449e1a110930f09116bd0d3cb9d
krishbee <kristoffer.frank@hotmail.com>
2022-09-15 11:00:22 +0200

    Person class added

---
+++ src/Test/Person.java
@@ -0,0 +1,13 @@
+package Test;
+
+public class Person {
+    String navn;
+
+    public Person(String navn){
+        this.navn = navn;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+}
--- src/Test/tester.java
+++ src/Test/tester.java
@@ -3,5 +3,9 @@ package Test;
 public class tester {
     public static void main(String[] args) {
         System.out.println("Hej verden!");
+        System.out.println("prøv igen");
+
+        Person person = new Person("Kris");
+        System.out.println("Hej " + person.getNavn());
     }
 }
